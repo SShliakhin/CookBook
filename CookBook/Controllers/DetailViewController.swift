dd7ec34ff705fd488cd6089fbe57a7aed5201500
nastyabeggin <45201186+nastyabeggin@users.noreply.github.com>
2022-12-02 20:51:21 +0300

    recomposition

--- CookBook/Controllers/DetailViewController.swift
+++ CookBook/Controllers/DetailViewController.swift
@@ -10,46 +10,148 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     // MARK: - Properties
-    private let ingredients = "cucumber 1 pc \n tomato 1 pc \n oil 1 tbsp" //for test
+    private var ingredients = [IngredientProtocol]()
     var addedToFavourites = false
 
     // MARK: - UI elements
-    private let recipeImageView: UIImageView = {
-        let image = UIImage(named: "test_recipe_image")
-        let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
+    private let mainStackView = UIStackView()
+    private let upperView = UIView()
     
-    private let shadowImageView: UIImageView = {
-        let image = UIImage(named: "shadow")
-        let imageView = UIImageView(image: image)
+    private let recipeImageView = UIImageView(image: UIImage(named: "test_recipe_image"))
+    private let shadowImageView = UIImageView(image: UIImage(named: "shadow"))
+    
+    private let recipeTableView = UITableView(frame: .zero, style: .plain)
+    private let recipeNameLabel = UILabel()
+    
+    private let detailStackView = UIStackView()
+
+    private lazy var addToFavouritesButton = UIButton()
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        setup()
+        applyStyle()
+        applyLayout()
+        
+        loadIngredients()
+        recipeTableView.dataSource = self
+        recipeTableView.delegate = self
+    }
+}
+// MARK: - Actions
+extension DetailViewController{
+    @objc private func addToFavourites() {
+        addedToFavourites = !addedToFavourites
+        let imageName = (addedToFavourites) ? "heart.fill" : "heart"
+        addToFavouritesButton.setImage(UIImage(systemName: imageName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 30, weight: .regular, scale: .medium)), for: .normal)
+    }
+}
+
+// MARK: - Style, layout and setup
+extension DetailViewController{
+    private func setup(){
+        recipeTableView.register(UITableViewCell.self, forCellReuseIdentifier: "detailCell")
+
+        addToFavouritesButton.addTarget(self, action: #selector(addToFavourites), for: .touchUpInside)
+    }
+    
+    private func applyStyle(){
+        applyStyleToImageView(for: recipeImageView)
+        applyStyleToImageView(for: shadowImageView)
+        
+        recipeTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        recipeNameLabel.text = "Simple salad"
+        recipeNameLabel.textColor = .white
+        recipeNameLabel.numberOfLines = 1
+        recipeNameLabel.minimumScaleFactor = 0.5
+        recipeNameLabel.font = UIFont.systemFont(ofSize: 27, weight: .semibold)
+        recipeNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        detailStackView.axis = .horizontal
+        detailStackView.alignment = .center
+        detailStackView.distribution = .equalSpacing
+                
+        addToFavouritesButton.setImage(UIImage(systemName: (addedToFavourites) ? "heart.fill" : "heart", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30, weight: .regular, scale: .medium)), for: .normal)
+        addToFavouritesButton.tintColor = .white
+        addToFavouritesButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        upperView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func applyLayout(){
+        upperView.addSubview(recipeImageView)
+        upperView.addSubview(shadowImageView)
+        upperView.addSubview(recipeNameLabel)
+        constructStackView()
+        upperView.addSubview(detailStackView)
+        upperView.addSubview(addToFavouritesButton)
+        arrangeStackView(
+                    for: mainStackView,
+                    subviews: [upperView, recipeTableView],
+                    spacing: 20
+            )
+        
+        view.addSubview(mainStackView)
+
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            upperView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            upperView.heightAnchor.constraint(equalToConstant: 350),
+            
+            recipeImageView.centerXAnchor.constraint(equalTo: upperView.centerXAnchor),
+            recipeImageView.bottomAnchor.constraint(equalTo: upperView.bottomAnchor),
+            recipeImageView.topAnchor.constraint(equalTo: upperView.topAnchor),
+
+            shadowImageView.centerXAnchor.constraint(equalTo: upperView.centerXAnchor),
+            shadowImageView.bottomAnchor.constraint(equalTo: recipeImageView.bottomAnchor),
+            
+            recipeNameLabel.widthAnchor.constraint(equalTo: upperView.widthAnchor, constant: -40),
+            recipeNameLabel.topAnchor.constraint(equalTo: upperView.topAnchor, constant: 220),
+            recipeNameLabel.leadingAnchor.constraint(equalTo: upperView.leadingAnchor, constant: 25),
+            
+            detailStackView.centerXAnchor.constraint(equalTo: upperView.centerXAnchor),
+            detailStackView.bottomAnchor.constraint(equalTo: recipeImageView.bottomAnchor),
+            detailStackView.widthAnchor.constraint(equalTo: recipeNameLabel.widthAnchor),
+            detailStackView.heightAnchor.constraint(equalToConstant: 80),
+            
+            addToFavouritesButton.trailingAnchor.constraint(equalTo: upperView.trailingAnchor, constant: -20),
+            addToFavouritesButton.topAnchor.constraint(equalTo: upperView.topAnchor, constant: 20),
+        ])
+    }
+    
+    // MARK: - Supporting methods
+    private func applyStyleToImageView(for imageView: UIImageView){
         imageView.contentMode = .scaleAspectFit
         imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
+    }
     
-    private let tableView: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "detailCell")
-        return table
-    }()
+    private func arrangeStackView(
+            for stackView: UIStackView,
+            subviews: [UIView],
+            spacing: CGFloat = 0,
+            axis: NSLayoutConstraint.Axis = .vertical,
+            distribution: UIStackView.Distribution = .fill,
+            aligment: UIStackView.Alignment = .fill
+        ) {
+            stackView.axis = axis
+            stackView.spacing = spacing
+            stackView.distribution = distribution
+            stackView.alignment = aligment
+            
+            subviews.forEach { item in
+                item.translatesAutoresizingMaskIntoConstraints = false
+                stackView.addArrangedSubview(item)
+            }
+        }
     
-    private let recipeNameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Simple salad"
-        label.textColor = .white
-        label.numberOfLines = 1
-        label.minimumScaleFactor = 0.5
-        label.font = UIFont.systemFont(ofSize: 27, weight: .semibold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let detailStackView: UIStackView = {
+    private func constructStackView(){
         // MARK: - Properties for stack view
         
         typealias Item = (name: String, value: Int)
@@ -67,17 +169,13 @@ final class DetailViewController: UIViewController {
         let nameStyle: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .thin), NSAttributedString.Key.foregroundColor: UIColor.white]
         
         // MARK: - StackView build
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .equalSpacing
         
         func buildSeparatorForStackView(){
             let separator = UIView()
             separator.widthAnchor.constraint(equalToConstant: 0.5).isActive = true
             separator.backgroundColor = .white
-            stackView.addArrangedSubview(separator)
-            separator.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.4).isActive = true
+            detailStackView.addArrangedSubview(separator)
+            separator.heightAnchor.constraint(equalTo: detailStackView.heightAnchor, multiplier: 0.4).isActive = true
         }
         
         func buildLabelForStackView(for item: Item){
@@ -89,128 +187,52 @@ final class DetailViewController: UIViewController {
             label.attributedText = totalText
             label.textAlignment = .center
             label.numberOfLines = 0
-            stackView.addArrangedSubview(label)
+            detailStackView.addArrangedSubview(label)
 
-            if let firstLabel = stackView.arrangedSubviews.first as? UILabel {
+            if let firstLabel = detailStackView.arrangedSubviews.first as? UILabel {
                 label.widthAnchor.constraint(equalTo: firstLabel.widthAnchor).isActive = true
             }
         }
         
         for item in items {
-            if stackView.arrangedSubviews.count > 0 {
+            if detailStackView.arrangedSubviews.count > 0 {
                 buildSeparatorForStackView()
             }
             buildLabelForStackView(for: item)
         }
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return stackView
-    }()
-    
-    private lazy var addToFavouritesButton: UIButton = {
-        let button = UIButton()
-        
-        let imageName = (addedToFavourites) ? "heart.fill" : "heart"
-        
-        button.setImage(UIImage(systemName: imageName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 30, weight: .regular, scale: .medium)), for: .normal)
-        button.tintColor = .white
-        
-        button.addTarget(self, action: #selector(addToFavourites), for: .touchUpInside)
-        
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    // MARK: - LifeCycle
-    override func viewDidLoad() {
-        setupHierarchy()
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tableView.frame = view.bounds
-    }
-    
-    // MARK: - Setups
-    private func setupHierarchy() {
-        view.addSubview(recipeImageView)
-        setupRecipeImageView()
-        view.addSubview(shadowImageView)
-        setupShadowImageView()
-        view.addSubview(recipeNameLabel)
-        setupRecipeNameLabel()
-        view.addSubview(detailStackView)
-        setupDetailStackView()
-        view.addSubview(addToFavouritesButton)
-        setupAddToFavouritesButton()
-        //view.addSubview(tableView)
-        //setupRecipeTableView()
-    }
-    
-    private func setupRecipeTableView(){
-        NSLayoutConstraint.activate([
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-    }
-    
-    private func setupRecipeImageView(){
-        NSLayoutConstraint.activate([
-            recipeImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-            recipeImageView.widthAnchor.constraint(equalToConstant: view.frame.width),
-            recipeImageView.heightAnchor.constraint(equalToConstant: 320)
-        ])
-    }
-    
-    private func setupShadowImageView(){
-        NSLayoutConstraint.activate([
-            shadowImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-            shadowImageView.bottomAnchor.constraint(equalTo: recipeImageView.bottomAnchor)
-        ])
-    }
-    
-    private func setupRecipeNameLabel(){
-        NSLayoutConstraint.activate([
-            recipeNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
-            recipeNameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 220),
-            recipeNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25)
-        ])
-    }
-    
-    private func setupDetailStackView(){
-        NSLayoutConstraint.activate([
-            detailStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-            detailStackView.bottomAnchor.constraint(equalTo: recipeImageView.bottomAnchor),
-            detailStackView.widthAnchor.constraint(equalTo: recipeNameLabel.widthAnchor),
-            detailStackView.heightAnchor.constraint(equalToConstant: 80)
-        ])
-    }
-    
-    private func setupAddToFavouritesButton(){
-        NSLayoutConstraint.activate([
-            addToFavouritesButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            addToFavouritesButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-        ])
-    }
-    
-    //MARK: - Actions
-    @objc private func addToFavourites() {
-        addedToFavourites = !addedToFavourites
-        let imageName = (addedToFavourites) ? "heart.fill" : "heart"
-        addToFavouritesButton.setImage(UIImage(systemName: imageName, withConfiguration: UIImage.SymbolConfiguration(pointSize: 30, weight: .regular, scale: .medium)), for: .normal)
+        detailStackView.translatesAutoresizingMaskIntoConstraints = false
     }
 }
 
 // MARK: - UITableView Data Source/Delegate
 extension DetailViewController: UITableViewDataSource{
+    // MARK: - Data loading
+    private func loadIngredients() {
+        ingredients.append(
+            IngredientModel(title: "cucumber", amount: "1 pc"))
+        ingredients.append(
+            IngredientModel(title: "tomato", amount: "1 pc"))
+        ingredients.append(
+            IngredientModel(title: "oil", amount: "1 tbsp"))
+        ingredients.sort{ $0.title < $1.title }
+    }
+    
+    private func configure(cell: inout UITableViewCell, for indexPath:
+    IndexPath) {
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = ingredients[indexPath.row].title
+        configuration.secondaryText = ingredients[indexPath.row].amount
+        cell.contentConfiguration = configuration
+    }
+    
+    // MARK: - Table properties
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return ingredients.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
-        cell.textLabel?.text = ingredients
+        var cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        configure(cell: &cell, for: indexPath)
         
         return cell
     }
